4b2801877a086beded7d62a30e1abdd72f28f2db
Jason Suarez <swarecito@gmail.com>
2024-04-23 13:34:28 +0200

    Refactor Nav component and add NavbarLogo

---
+++ src/common/components/NavBarLogo.tsx
@@ -0,0 +1,10 @@
+const NavbarLogo = () => {
+  return (
+    <div className="flex items-center bg-[#070F2B] p-2">
+      <img src="logo.webp" alt="RequestStuff Logo" className="h-10 mr-3" />
+      <h1 className="text-lg font-bold text-[#9290C3]">RequestStuff</h1>
+    </div>
+  );
+};
+
+export default NavbarLogo;
