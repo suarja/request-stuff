6a9c639346efb5dc5fcee29a620c081ad30341e4
Jason Suarez <swarecito@gmail.com>
2024-04-23 13:07:59 +0200

    Refactor nav and add comments todo later on

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ModeToggle } from "@/common/components/ModeToggle";
+import { Nav } from "@/common/components/Nav";
 import { useAuthMiddleware } from "@/context/useAuthMiddleware";
 import Link from "next/link";
 
@@ -7,25 +8,7 @@ function Page(): JSX.Element {
   useAuthMiddleware();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="/signin">
-            <span className="link">Login</span>
-          </Link>
-        </div>
-
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="/signup">
-            <span className="link">Register</span>
-          </Link>
-        </div>
-        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <ModeToggle />
-        </div>
-      </nav>
-      <div>
-        <h1>Request/Stuff</h1>
-      </div>
+      <Nav />
     </main>
   );
 }
--- src/context/useAuthMiddleware.ts
+++ src/context/useAuthMiddleware.ts
@@ -1,18 +1,20 @@
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useAuthMiddleware() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState<boolean | null>(null);
   // Access the user object from the authentication context
   // const { user } = useAuthContext();
+
+  //! Find a way to avoid the any type and add a type to the user object
   const { user } = useAuthContext() as { user: any }; // Use 'as' to assert the type as { user: any }
   const router = useRouter();
 
+  //! TODO: Redirect to login page if user is not logged in
   useEffect(() => {
-    // Redirect to the home page if the user is not logged in
     if (user == null) {
       router.push("/");
     }
-    // }, [ user ] );
   }, [user, router]); // Include 'router' in the dependency array to resolve eslint warning
 }
