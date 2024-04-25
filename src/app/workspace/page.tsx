1abae7ce598c928181f13bb9649287488e196e59
Jason Suarez <swarecito@gmail.com>
2024-04-25 22:38:21 +0200

    Refactor workspace page component to display path content from file repository

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,13 +1,42 @@
-"use client";
-import useGetUserFiles from "@/features/file/application/usecases/services/useGetUserFIles";
+import { fileRepositoryImplementation } from "@/features/file/infra/file-repository-impl";
 import FolderTree from "@/features/file/presentation/components/Foldertree";
 
-export default function Page() {
-  const { files, loading } = useGetUserFiles();
-  if (loading) return <div>Loading...</div>;
+export default async function Page() {
+  const pathContent = await fileRepositoryImplementation.getPathContent({
+    path: "users/sU69jGBlKNU66jc9ZKPKmnnoJkh1/files",
+  });
+  console.log({ pathContent });
+  console.log({ prefixes: pathContent.prefixes, items: pathContent.items });
+  // const { files, loading } = useGetUserFiles();
+  // if (loading) return <div>Loading...</div>;
   return (
-    <>
-      <FolderTree folders={[{ name: "Root", files }]} title="Files" />
-    </>
+    <div>
+      <h1 className="text-xl">Prefixes</h1>
+      <br />
+      {pathContent.prefixes.map((prefix, index) => (
+        <div key={index}>
+          <p>{index + 1}</p>
+          <span>Name: </span>
+          <span>{prefix.name}</span>
+          <br />
+          <span>Full Path: </span>
+          <span>{prefix.fullPath}</span>
+          <br />
+          <div>
+            {pathContent.items.map((item, index) => (
+              <div key={index}>
+                <p>{index + 1}</p>
+                <span>Name: </span>
+                <span>{item.name}</span>
+                <br />
+                <span>Full Path: </span>
+                <span>{item.fullPath}</span>
+                <br />
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
