434204d063852a264cf56d342040eb77179c84d9
Jason Suarez <swarecito@gmail.com>
2024-04-27 00:33:05 +0200

    Implement get request by user

---
+++ src/features/request/application/usecases/services/useGetRequestsByUser.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { requestUsecases } from "../request-usecases";
+import { RequestBase } from "@/features/request/domain/entities/request-types";
+
+export default function useGetRequestsByUser() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [requests, setRequests] = useState<RequestBase[] | null>(null);
+
+  useEffect(() => {
+    if (userId) {
+      setLoading(true);
+
+      requestUsecases
+        .getRequestsByUser({ userId })
+        .then((data) => {
+          setRequests(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError("Error getting requests");
+          console.log(error);
+
+          setLoading(false);
+        });
+    }
+  }, [userId]);
+
+  return {
+    loading,
+    error,
+    setUserId,
+    requests,
+  };
+}
