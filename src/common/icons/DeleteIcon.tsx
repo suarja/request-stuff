01083f24be131ab16c4b8848b26f2182843a6e13
Jason Suarez <swarecito@gmail.com>
2024-04-27 02:24:07 +0200

    Refactor DeleteIcon component to accept props and remove unnecessary code.

--- src/common/icons/DeleteIcon.tsx
+++ src/common/icons/DeleteIcon.tsx
@@ -1,12 +1,14 @@
-export default function DeleteIcon() {
+import { ReactSVG } from "react";
+
+export default function DeleteIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
     >
       <path
         strokeLinecap="round"
