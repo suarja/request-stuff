551e5a756eb63af397ba0c662089a1ed455cb610
Jason Suarez <swarecito@gmail.com>
2024-04-27 02:42:33 +0200

    Refactor Page component to use UploadCard directly

--- src/app/upload/[slug]/page.tsx
+++ src/app/upload/[slug]/page.tsx
@@ -4,9 +4,5 @@ export interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 export default function Page({ searchParams }: PageProps) {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <UploadCard searchParams={searchParams} />
-    </div>
-  );
+  return <UploadCard searchParams={searchParams} />;
 }
