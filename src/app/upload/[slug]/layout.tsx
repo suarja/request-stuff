c57caebe72d283f8189ee43cbc290178fe0a157f
Jason Suarez <swarecito@gmail.com>
2024-04-27 14:02:58 +0200

    Update layout styles in upload/[slug]/layout.tsx

--- src/app/upload/[slug]/layout.tsx
+++ src/app/upload/[slug]/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <main className="flex min-h-screen w-full flex-col gap-12 items-center  px-4 py-24  ">
+    <main className="flex min-h-screen w-full flex-col gap-12 items-center py-12 px-4 md:py-24  ">
       <div className="flex flex-col gap-8">
         <nav className="flex h-full w-full justify-start  ">
           <NavbarLogo />
