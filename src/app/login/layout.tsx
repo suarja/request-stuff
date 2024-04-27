0ed713ce4e98eeffbc260feb9f0fa42a3144bde2
Jason Suarez <swarecito@gmail.com>
2024-04-27 12:31:28 +0200

    Add layout page to login route

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,19 @@
+import { Nav } from "@/common/components/Nav";
+
+export const metadata = {
+  title: "RequestStuff",
+  description:
+    "RequestStuff: Streamline Your File Requests Without the High Costs. RequestStuff allows seamless file uploads via a simple link, directly storing each file securely in our database. Designed for businesses, educators, and freelancers seeking an efficient, affordable solution for managing document submissions. Experience streamlined file sharing",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <main className="flex min-h-screen w-full flex-col gap-12 items-center justify-start px-4 py-24  ">
+      {children}
+    </main>
+  );
+}
