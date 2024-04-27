b3facd1d646424606b1a8aa388101b0b893f5559
Jason Suarez <swarecito@gmail.com>
2024-04-27 14:02:15 +0200

    Add layout page to upload path

---
+++ src/app/upload/[slug]/layout.tsx
@@ -0,0 +1,25 @@
+import { Nav } from "@/common/components/Nav";
+import NavbarLogo from "@/common/components/NavBarLogo";
+
+export const metadata = {
+  title: "RequestStuff",
+  description:
+    "RequestStuff: Streamline Your File Requests Without the High Costs. RequestStuff allows seamless file uploads via a simple link, directly storing each file securely in our database. Designed for businesses, educators, and freelancers seeking an efficient, affordable solution for managing document submissions. Experience streamlined file sharing",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <main className="flex min-h-screen w-full flex-col gap-12 items-center  px-4 py-24  ">
+      <div className="flex flex-col gap-8">
+        <nav className="flex h-full w-full justify-start  ">
+          <NavbarLogo />
+        </nav>
+        {children}
+      </div>
+    </main>
+  );
+}
