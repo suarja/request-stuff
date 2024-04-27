d2b60adfa3d6ece416609eeedb7dd9eca8a12f87
Jason Suarez <swarecito@gmail.com>
2024-04-27 04:52:58 +0200

    Refactor useHandleFolderState to use "Requests" as the query parameter instead of the dynamic "name" parameter

--- src/features/file/application/usecases/services/useHandleFolderState.ts
+++ src/features/file/application/usecases/services/useHandleFolderState.ts
@@ -2,27 +2,27 @@ import { useSearchParams, usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
-export default function useHandleFolderState(name: string) {
+export default function useHandleFolderState() {
   const searchParams = useSearchParams();
-  const [open, setOpen] = useState(() => searchParams.get(name) === "true");
+  const [open, setOpen] = useState(
+    () => searchParams.get("Requests") === "true"
+  );
   const router = useRouter();
   const pathName = usePathname();
 
   useEffect(() => {
-    const isInUrl = searchParams.has(name);
-    if (!isInUrl) {
-      const newParams = new URLSearchParams(searchParams.toString());
-      newParams.set(name, open.toString());
-      router.replace(`${pathName}?`);
-    }
+
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set("Requests", "false");
+    router.replace(`${pathName}?${newParams.toString()}`);
   }, []);
 
   function handleToggle() {
-    const isOpen = searchParams.get(name) === "true";
+    const isOpen = searchParams.get("Requests") === "true";
     setOpen(!isOpen);
 
     const newParams = new URLSearchParams(searchParams.toString());
-    newParams.set(name, (!isOpen).toString());
+    newParams.set("Requests", (!isOpen).toString());
     router.replace(`
       ${pathName}?${newParams.toString()}`);
   }
