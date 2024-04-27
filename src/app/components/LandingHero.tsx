4b459afc96e0f7c522db08a4ed59a4e195f58e69
Jason Suarez <swarecito@gmail.com>
2024-04-27 13:17:29 +0200

    Add Next.js Image component to LandingHero for better image optimization

--- src/app/components/LandingHero.tsx
+++ src/app/components/LandingHero.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/common/components/ui/button";
 import PaperclipIcon from "@/common/icons/PaperClipIcon";
+import Image from "next/image";
 import Link from "next/link";
 
-
 export default function LandingHero() {
-    return (
-      
-      <section className="w-full py-20 md:py-32 bg-gray-50 dark:bg-primary">
+  return (
+    <section className="w-full py-20 md:py-32 bg-gray-50 dark:bg-primary">
       <div className="container px-4 md:px-6 grid md:grid-cols-2 gap-8 items-center">
         <div className="space-y-6">
           <h1 className="text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl">
@@ -22,8 +21,9 @@ export default function LandingHero() {
           </div>
         </div>
         <div className="flex justify-center">
-          <img
-            alt="RequestStuff Hero"
+          <Image
+            priority
+            alt="RequestStuff Hero Image"
             className="rounded-lg"
             height="400"
             src="/hero.webp"
@@ -36,5 +36,5 @@ export default function LandingHero() {
         </div>
       </div>
     </section>
-    );
+  );
 }
