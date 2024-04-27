1c02aa4069aa1f2dfc0fe0241f56bebf7b13b0cf
Jason Suarez <swarecito@gmail.com>
2024-04-27 13:09:29 +0200

    Add Suspense component to improve loading state in dashboard/[...slug]/page.tsx

--- src/app/dashboard/[...slug]/page.tsx
+++ src/app/dashboard/[...slug]/page.tsx
@@ -1,7 +1,9 @@
+import LoadingPage from "@/common/components/LoadingPage";
 import { fileUsecases } from "@/features/file/application/usecases/file-usecases";
 import { requestUsecases } from "@/features/request/application/usecases/request-usecases";
 import RequestGrid from "@/features/request/presentation/views/RequestGrid";
 import { cookies } from "next/headers";
+import { Suspense } from "react";
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const session = cookies().get("session")?.value || "";
 
@@ -15,6 +17,8 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
   const requests = await requestUsecases.getRequestsByUser({ userId });
 
   return (
-    <RequestGrid requests={requests} params={path} />
+    <Suspense fallback={<LoadingPage />}>
+      <RequestGrid requests={requests} params={path} />
+    </Suspense>
   );
 }
