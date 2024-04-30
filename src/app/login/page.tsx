590adf541337dd8479d2cc6176e6b996689d67de
Jason Suarez <swarecito@gmail.com>
2024-04-30 19:31:19 +0200

    Move chrome icon to icons folder

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { BASE_URL } from "@/common/constants";
 import LoadingIcon from "@/common/icons/LoadingIcon";
+import ChromeIcon from "@/common/icons/ChromeIcon";
 
 export default function Component() {
   const [email, setEmail] = useState("");
@@ -114,28 +115,5 @@ export default function Component() {
         </div>
       </div>
     </div>
-  );
-}
-
-function ChromeIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <circle cx="12" cy="12" r="4" />
-      <line x1="21.17" x2="12" y1="8" y2="8" />
-      <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
-      <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
-    </svg>
   );
 }
