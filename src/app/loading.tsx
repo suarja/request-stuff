82944b2ccb949ba70591e9539cdcd4047e998eeb
Jason Suarez <swarecito@gmail.com>
2024-05-03 22:09:59 +0200

    Refactor loading component to use LoadingSkeletonPage for better UI experience

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,6 @@
+import LoadingSkeletonPage from "@/common/components/LoadingSkeletonPage";
 import { Skeleton } from "@/common/components/ui/skeleton";
 
 export default async function Page() {
-  return (
-    <div className="flex flex-col  justify-between h-screen">
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-    </div>
-  );
+  return <LoadingSkeletonPage />;
 }
