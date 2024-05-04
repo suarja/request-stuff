db23c7541d7379ae9a03ea0083f9a252feb50f13
Jason Suarez <swarecito@gmail.com>
2024-05-04 22:35:45 +0200

    Update maxFiles validation in RequestSchema to include a security limit

--- src/features/request/domain/entities/request-schema.ts
+++ src/features/request/domain/entities/request-schema.ts
@@ -4,6 +4,10 @@ export const RequestSchema = z.object({
   name: z.string().min(1, "Name is required"),
   description: z.string().optional(),
   maxFileSize: z.number().positive().optional(),
-  maxFiles: z.number().positive().optional(),
+  maxFiles: z
+    .number()
+    .positive()
+    .max(100, "This is for security reasons")
+    .optional(),
   dateLimit: z.string().optional(),
 });
