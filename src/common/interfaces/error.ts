0b753e23595a0a1b0fe6adb569a0f605aac408ba
Jason Suarez <swarecito@gmail.com>
2024-05-05 19:17:05 +0200

    Update error message interface and add new error types

--- src/common/interfaces/error.ts
+++ src/common/interfaces/error.ts
@@ -1,8 +1,12 @@
 export type ErrorMessage<T extends string = ""> =
   | `File not found ${T}`
+  | "User not found"
+  | `File is too big. ${T}`
+  | `Bad request. ${T}`
   | `Max number of files reached. ${T}`
   | `Request date limit exceeded. ${T}`
   | "File already exists"
   | "Could not add file to request beacuse could not get user"
   | `Space available insuficient ${T}`
-  | "An unknown error happend";
+  | "An unknown error happend"
+  | "File uploaded successfully.";
