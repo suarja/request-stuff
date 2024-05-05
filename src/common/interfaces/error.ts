a5e5933b1398b542f5625117f269db882bb3b4eb
Jason Suarez <swarecito@gmail.com>
2024-05-05 17:06:00 +0200

    Add error message interface

---
+++ src/common/interfaces/error.ts
@@ -0,0 +1,10 @@
+export type ErrorMessage =
+  | "File not found"
+  | "Error uploading file"
+  | "Error saving file"
+  | "Error sending file"
+  | "Error sending email"
+  | "Error saving request"
+  | "Error saving request data"
+  | "Error saving file data"
+  | "Error saving file sender";
