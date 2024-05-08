5e4dd9b8799d23853b4c02dbfe91425bfd373906
Jason Suarez <swarecito@gmail.com>
2024-05-08 12:35:59 +0200

    Refactor error messages and file upload logic

--- src/common/interfaces/error.ts
+++ src/common/interfaces/error.ts
@@ -2,11 +2,15 @@ export type ErrorMessage<T extends string = ""> =
   | `File not found ${T}`
   | "User not found"
   | `File is too big. ${T}`
+  | "File is too big"
   | `Bad request. ${T}`
   | `Max number of files reached. ${T}`
   | `Request date limit exceeded. ${T}`
+  | `Request date limit exceeded`
   | "File already exists"
   | "Could not add file to request beacuse could not get user"
   | `Space available insuficient ${T}`
   | "An unknown error happend"
-  | "File uploaded successfully.";
+  | "File uploaded successfully."
+  | "Request is not valid"
+  | "No error";
--- src/features/request/domain/entities/request-entity.ts
+++ src/features/request/domain/entities/request-entity.ts
@@ -2,6 +2,7 @@ import { Entity, Failure } from "fp-ddd";
 import { PublicRequest, Upload } from "./request-types";
 import { Either, left, right } from "fp-ts/lib/Either";
 import { publicRequestSchema } from "./request-schema";
+import { ErrorMessage } from "@/common/interfaces/error";
 
 export default class PublicRequestEntity extends Entity<PublicRequest> {
   readonly values: Either<Failure<PublicRequest>, PublicRequest>;
@@ -43,9 +44,86 @@ export default class PublicRequestEntity extends Entity<PublicRequest> {
     const senderNameHashed = senderName + fileName;
     const upload: Upload = {
       fileName,
-      senderHash: senderNameHashed,
+      senderHash: this.senderNameHash({ fileName, senderName }),
       senderIp: ip,
     };
     return upload;
+  }
+
+  senderNameHash({
+    fileName,
+    senderName,
+  }: {
+    fileName: string;
+    senderName: string;
+  }) {
+    return senderName + fileName;
+  }
+
+  canUploadFile({ file, senderName }: { file: File; senderName: string }): {
+    canUpload: boolean;
+    message: ErrorMessage;
+    additionalInfo?: string;
+  } {
+    // check ip address
+
+    //~Check if this is valid
+    if (!this.isValid()) {
+      return { canUpload: false, message: "Request is not valid" };
+    }
+
+    const request = this.getOrCrash();
+    // Check file size
+    const fileSizeInMb = (file as File).size / 1024 ** 2;
+    if (fileSizeInMb > request.maxFileSize!) {
+      return {
+        canUpload: false,
+        message: `File is too big`,
+        additionalInfo: `File should be less than: ${request.maxFileSize!}`,
+      };
+    }
+
+    // check upload time
+    const dateLimitFormatted = new Date(request.dateLimit!).getTime();
+    if (Date.now() > dateLimitFormatted) {
+      return {
+        canUpload: false,
+        message: `Request date limit exceeded`,
+        additionalInfo: `Dead line was:  ${new Date(
+          request.dateLimit!
+        ).toLocaleDateString()}`,
+      };
+    }
+
+    // check if file already exists
+    const fileExists = request.uploads.some((upload) => {
+      if (
+        upload.fileName === file.name &&
+        upload.senderHash ===
+          this.senderNameHash({ fileName: file.name, senderName })
+      ) {
+        return true;
+      }
+    });
+
+    if (fileExists) {
+      return {
+        canUpload: false,
+        message: "File already exists",
+      };
+    }
+
+    // check number of uploads
+    if (request.numberOfUploads + 1 > request.maxFiles!) {
+      return {
+        canUpload: false,
+        message: "Max number of files reached. ",
+      };
+    }
+
+    return {
+      canUpload: true,
+      message: "No error",
+    };
   }
 }
