46c4c19ac6f59b30453ee8809ed289ae23f750e9
Jason Suarez <swarecito@gmail.com>
2024-05-08 14:39:47 +0200

    refactor: Improve file upload logic and error messages in upload route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -18,159 +18,166 @@ import { NextRequest, NextResponse } from "next/server";
 customInitApp();
 
 export async function POST(request: NextRequest, response: NextResponse) {
-  const ip = request.headers.get("x-forwarded-for");
-  const form = await request.formData();
-  const file = form.get("file");
-  const fileSenderData: FileSenderData = JSON.parse(
-    form.get("fileSenderData") as string
-  );
-  const requestData: PublicRequest = JSON.parse(
-    form.get("requestData") as string
-  );
-  const publicRequest = PublicRequestEntity.create(requestData);
-
-  //~ Check if request options
-  if (!requestData || fileSenderData === undefined || !file) {
-    const message: ErrorMessage<""> = "Bad request. ";
-    return NextResponse.json(
-      {
-        message,
-        error: true,
-      },
-      { status: 200 }
+  try {
+    const ip = request.headers.get("x-forwarded-for");
+    const form = await request.formData();
+    const file = form.get("file");
+    const fileSenderData: FileSenderData = JSON.parse(
+      form.get("fileSenderData") as string
     );
-  }
-
-  const canUploadFile = publicRequest.canUploadFile({
-    file: file as File,
-    senderName: fileSenderData.senderName,
-  });
-  if (!canUploadFile.canUpload) {
-    return NextResponse.json(
-      {
-        message: canUploadFile.message,
-        error: true,
-        additionalInfo: canUploadFile.additionalInfo,
-      },
-      { status: 200 }
+    const requestData: PublicRequest = JSON.parse(
+      form.get("requestData") as string
     );
-  }
+    const publicRequest = PublicRequestEntity.create(requestData);
 
-  //~ check user has enough ressources (subscription plan, storage available)
-  const userId = requestData.userId;
-  const userInfra = await firebaseAdmin.getDocument("users", userId);
-  if (!userInfra) {
-    const message: ErrorMessage<""> = "User not found";
-    return NextResponse.json(
-      {
-        message,
-        error: true,
-      },
-      { status: 200 }
+    //~ Check if request options
+    if (!requestData || fileSenderData === undefined || !file) {
+      const message: ErrorMessage<""> = "Bad request. ";
+      return NextResponse.json(
+        {
+          message,
+          error: true,
+        },
+        { status: 200 }
+      );
+    }
+
+    const canUploadFile = publicRequest.canUploadFile({
+      file: file as File,
+      senderName: fileSenderData.senderName,
+    });
+    if (!canUploadFile.canUpload) {
+      return NextResponse.json(
+        {
+          message: canUploadFile.message,
+          error: true,
+          additionalInfo: canUploadFile.additionalInfo,
+        },
+        { status: 200 }
+      );
+    }
+
+    //~ check user has enough ressources (subscription plan, storage available)
+    const userId = requestData.userId;
+    const userInfra = await firebaseAdmin.getDocument("users", userId);
+    if (!userInfra) {
+      const message: ErrorMessage<""> = "User not found";
+      return NextResponse.json(
+        {
+          message,
+          error: true,
+        },
+        { status: 200 }
+      );
+    }
+
+    const userDto = new UserDto();
+    const eitherUser = userDto.toDomain({ data: userInfra });
+    if (isLeft(eitherUser)) {
+      const message: ErrorMessage<""> =
+        "Could not add file to request beacuse could not get user";
+      return NextResponse.json(
+        {
+          message,
+          error: true,
+        },
+        { status: 200 }
+      );
+    }
+    const user = eitherUser.right;
+    const userCanUpload = user.canUploadFile({
+      file: file as File,
+    });
+
+    if (!userCanUpload.canUpload) {
+      return NextResponse.json(
+        {
+          message: userCanUpload.message,
+          error: true,
+        },
+        { status: 200 }
+      );
+    }
+
+    //* Upload file
+    // 1. Upload file to storage
+    const fileUrl = await firebaseAdmin.uploadFile(
+      file as File,
+      `users/${user.getOrCrash().id}/requests/${
+        publicRequest.getOrCrash().id
+      }/files/${(file as File).name}`
     );
-  }
+    if (!fileUrl) {
+      const message: ErrorMessage<""> = "An unknown error happend";
+      return NextResponse.json(
+        {
+          message,
+          error: true,
+        },
+        { status: 200 }
+      );
+    }
 
-  const userDto = new UserDto();
-  const eitherUser = userDto.toDomain({ data: userInfra });
-  if (isLeft(eitherUser)) {
-    const message: ErrorMessage<""> =
-      "Could not add file to request beacuse could not get user";
-    return NextResponse.json(
-      {
-        message,
-        error: true,
+    // 2. Update public request collection
+    const upload: Upload = publicRequest.createUpload({
+      fileName: (file as File).name,
+      senderName: fileSenderData.senderName,
+      ip: ip ?? "",
+    });
+    await firebaseAdmin.updateArray({
+      collection: "requests",
+      id: requestData.id,
+      field: "uploads",
+      data: {
+        ...upload,
       },
-      { status: 200 }
-    );
-  }
-  const user = eitherUser.right;
-  const userCanUpload = user.canUploadFile({
-    file: file as File,
-  });
-
-  if (!userCanUpload.canUpload) {
-    return NextResponse.json(
-      {
-        message: userCanUpload.message,
-        error: true,
+      updateRest: true,
+      rest: {
+        numberOfUploads: requestData.numberOfUploads + 1,
       },
-      { status: 200 }
-    );
-  }
+    });
 
-  //* Upload file
-  // 1. Upload file to storage
-  const fileUrl = await firebaseAdmin.uploadFile(
-    file as File,
-    `users/${user.getOrCrash().id}/requests/${
-      publicRequest.getOrCrash().id
-    }/files/${(file as File).name}`
-  );
-  if (!fileUrl) {
-    const message: ErrorMessage<""> = "An unknown error happend";
-    return NextResponse.json(
-      {
-        message,
-        error: true,
-      },
-      { status: 200 }
-    );
-  }
-
-  // 2. Update public request collection
-  const upload: Upload = publicRequest.createUpload({
-    fileName: (file as File).name,
-    senderName: fileSenderData.senderName,
-    ip: ip ?? "",
-  });
-  await firebaseAdmin.updateArray({
-    collection: "requests",
-    id: requestData.id,
-    field: "uploads",
-    data: {
-      ...upload,
-    },
-    updateRest: true,
-    rest: {
-      numberOfUploads: requestData.numberOfUploads + 1,
-    },
-  });
-
-  // 3. Update request in user collection
-  const userUpload: UserUpload = {
-    fileName: (file as File).name,
-    fileUrl,
-    fileSenderData: {
-      ...fileSenderData,
+    // 3. Update request in user collection
+    const userUpload: UserUpload = {
+      fileName: (file as File).name,
       fileUrl,
-    },
-  };
-  await firebaseAdmin.updateArray({
-    collection: `users/${user.getOrCrash().id}/requests`,
-    id: requestData.id,
-    field: "uploads",
-    data: {
-      ...userUpload,
-    },
-    updateRest: true,
-    rest: {
-      numberOfUploads: requestData.numberOfUploads + 1,
-    },
-  });
+      fileSenderData: {
+        ...fileSenderData,
+        fileUrl,
+      },
+    };
+    await firebaseAdmin.updateArray({
+      collection: `users/${user.getOrCrash().id}/requests`,
+      id: requestData.id,
+      field: "uploads",
+      data: {
+        ...userUpload,
+      },
+      updateRest: true,
+      rest: {
+        numberOfUploads: requestData.numberOfUploads + 1,
+      },
+    });
 
-  // 4. Update user
-  const fileSizeInMb = (file as File).size / 1024 ** 2;
-  await firebaseAdmin.updateDocument("users", user.getOrCrash().id, {
-    currentStorage: user.getOrCrash().currentStorage + fileSizeInMb,
-  });
+    // 4. Update user
+    const fileSizeInMb = (file as File).size / 1024 ** 2;
+    await firebaseAdmin.updateDocument("users", user.getOrCrash().id, {
+      currentStorage: user.getOrCrash().currentStorage + fileSizeInMb,
+    });
 
-  const message: ErrorMessage<""> = "File uploaded successfully.";
-  return NextResponse.json(
-    {
-      message,
-      error: false,
-      fileUrl,
-    },
-    { status: 200 }
-  );
+    const message: ErrorMessage<""> = "File uploaded successfully.";
+    return NextResponse.json(
+      {
+        message,
+        error: false,
+        fileUrl,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({
+      message: "An unknown error happened",
+      error: true,
+    });
+  }
 }
