ebf4db5ce51b3d95e10c786737efb9ceaebffa2b
Jason Suarez <swarecito@gmail.com>
2024-05-09 20:09:59 +0200

    refactor: Add upload functionality to ServerAdapter

--- src/features/server/application/adapters/server-adapter.ts
+++ src/features/server/application/adapters/server-adapter.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import ServerUsecases from "../usecases/server-usecases";
 import UserEntity from "@/features/auth/domain/entities/user-entity";
+import PublicRequestEntity from "@/features/request/domain/entities/request-entity";
+import { Upload } from "@/features/request/domain/entities/request-types";
 
 @injectable()
 export default class ServerAdapter {
@@ -61,6 +63,39 @@ export default class ServerAdapter {
     return {
       returnOptions: { error: false, message: "", status: 200 },
       url: result.url,
+    };
+  }
+
+  async addUploadToPublicRequest({
+    ip,
+    senderName,
+    fileName,
+    request,
+  }: {
+    request: PublicRequestEntity;
+    ip: string;
+    senderName: string;
+    fileName: string;
+  }): Promise<{
+    returnOptions: { error: boolean; message: string; status: number };
+  }> {
+    const result = await this._usecases.addUploadToPublicRequest({
+      request,
+      ip,
+      senderName,
+      fileName,
+    });
+    if (result.error) {
+      return {
+        returnOptions: {
+          error: true,
+          message: result.message,
+          status: 200,
+        },
+      };
+    }
+    return {
+      returnOptions: { error: false, message: "", status: 200 },
     };
   }
 }
