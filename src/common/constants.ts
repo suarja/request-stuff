d0261a5c1be9f05ad77b5201dcf8028e42c487c7
Jason Suarez <swarecito@gmail.com>
2024-05-09 19:31:46 +0200

    refactor: Update constants.ts to include new paths for user and request URLs

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,3 +2,16 @@ export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : "https://request-stuff.vercel.app";
+
+export const PATHS = {
+  USER: ({ userId }: { userId: string }) => `users/${userId}`,
+  USER_REQUEST: ({
+    userId,
+    requestId,
+  }: {
+    userId: string;
+    requestId: string;
+  }) => `users/${userId}/requests/${requestId}`,
+  PUBLIC_REQUEST: ({ requestId }: { requestId: string }) =>
+    `requests/${requestId}`,
+};
