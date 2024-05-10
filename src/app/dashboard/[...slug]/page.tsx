e4d79393ec157285514689c5425080bb2712b67f
Jason Suarez <swarecito@gmail.com>
2024-05-10 23:38:00 +0200

    feat: Add error handling for failed request retrieval

--- src/app/dashboard/[...slug]/page.tsx
+++ src/app/dashboard/[...slug]/page.tsx
@@ -2,6 +2,7 @@ import LoadingPage from "@/common/components/LoadingPage";
 import { fileUsecases } from "@/features/file/application/usecases/file-usecases";
 import { requestUsecases } from "@/features/request/application/usecases/request-usecases";
 import RequestGrid from "@/features/request/presentation/views/RequestGrid";
+import { isLeft } from "fp-ts/lib/Either";
 import { cookies } from "next/headers";
 import { Suspense } from "react";
 export default async function Page({ params }: { params: { slug: string[] } }) {
@@ -14,8 +15,12 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
   if (!userId) {
     return <div>Not logged in</div>;
   }
-  const requests = await requestUsecases.getRequestsByUser({ userId });
-
+  const eitherRequests = await requestUsecases.getRequestsByUser({ userId });
+  if (isLeft(eitherRequests)) {
+    console.log(eitherRequests.left);
+    return <div>Error getting requests</div>;
+  }
+  const requests = eitherRequests.right;
   return (
     <Suspense fallback={<LoadingPage />}>
       <RequestGrid requests={requests} params={path} />
