cf64ee87b41a963ecdcce83fd964d33c7cef8cbb
Jason Suarez <swarecito@gmail.com>
2024-05-10 23:35:32 +0200

    Add firebase emulators

--- src/common/data/firebase/config.ts
+++ src/common/data/firebase/config.ts
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { initializeApp, getApps } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +14,27 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+export function shouldConnectFirestoreEmulator(): boolean {
+  return process.env.NODE_ENV === "development";
+}
+
+export function getFirestoreEmulatorHost() {
+  return process.env.NEXT_PUBLIC_FIRESTORE_EMULATOR_HOST ?? "localhost";
+}
+export function getFirestoreEmulatorPort() {
+  return Number(process.env.NEXT_PUBLIC_FIRESTORE_EMULATOR_PORT) ?? 8080;
+}
 // Initialize Firebase
-let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let firebase_app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+  
+export const FirestoreDB = getFirestore(firebase_app);
+if (shouldConnectFirestoreEmulator()) {
+  connectFirestoreEmulator(
+    FirestoreDB,
+    getFirestoreEmulatorHost(),
+    getFirestoreEmulatorPort()
+  );
+}
 
 export default firebase_app;
