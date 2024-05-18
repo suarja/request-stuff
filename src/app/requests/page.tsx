fbd27f5270025dd880d8b8da9ea7f1004846fc50
Jason Suarez <swarecito@gmail.com>
2024-05-18 18:36:36 +0200

    refactor: Add page component for requests handling

---
+++ src/app/requests/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { BASE_URL } from "@/common/constants";
+
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [authState, setAuthState] = useState({
+    isAuthenticated: false,
+  });
+
+  useEffect(() => {
+    async function check() {
+      const authPayload = await fetch(`${BASE_URL}/api/requests`, {
+        method: "POST",
+        headers: {},
+      });
+      const authData = await authPayload.json();
+      console.log(authData);
+      return authData;
+    }
+
+    check().then((data) => {
+      setAuthState({
+        isAuthenticated: data.isAuthenticated,
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Page</h1>
+      <p> isAuthenticated {authState.isAuthenticated ? "true" : " false"}</p>
+    </div>
+  );
+}
