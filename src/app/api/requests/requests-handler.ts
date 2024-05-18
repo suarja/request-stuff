e7e6d033c2097f1fdbeb8c53a4cb1c4bcfa09890
Jason Suarez <swarecito@gmail.com>
2024-05-18 19:41:54 +0200

    refactor: Update requestHandler to include request routing and authentication check

--- src/app/api/requests/requests-handler.ts
+++ src/app/api/requests/requests-handler.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { serverAdapter } from "../upload/dependency-injection";
+import { requestRouter, serverAdapter } from "../upload/dependency-injection";
 import { isLeft } from "fp-ts/lib/Either";
 import { cookies } from "next/headers";
 
@@ -13,15 +13,18 @@ export async function requestHandler(
     cookie,
   });
   if (isLeft(eitherUserAuthentication)) {
-    // console.log("Error: ", eitherUserAuthentication.left.body);
     return eitherUserAuthentication.left;
   }
-  //! check if the user is authorized to make the request
-  //! Check the target of the request (e.g. addPublicRequest, addPrivateRequest, etc.)
 
+  return await requestRouter.handler(request);
   //! Invoke the usecase
+
+  //~ check if the user is authorized to make the request
 
   //~ Check if the request is valid in each usecase
 
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  return NextResponse.json(
+    { message: "Hello World", isAuthenticated: true },
+    { status: 200 }
+  );
 }
