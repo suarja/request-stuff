83a9d0ac8b260c5ce1241803ccde98427603ca3f
Jason Suarez <swarecito@gmail.com>
2024-05-19 17:40:17 +0200

    refactor: Remove commented out code in requestHandler

--- src/app/api/requests/requests-handler.ts
+++ src/app/api/requests/requests-handler.ts
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { requestRouter, serverAdapter } from "../upload/dependency-injection";
-import { isLeft } from "fp-ts/lib/Either";
-import { cookies } from "next/headers";
+import { requestRouter } from "../upload/dependency-injection";
 
 export async function requestHandler(
   request: NextRequest
 ): Promise<NextResponse> {
   // Rate limiting
+  console.log("Request Handler");
 
-  const cookie = cookies().get("session")?.value || "";
-  const eitherUserAuthentication = await serverAdapter.userAuthentication({
-    cookie,
-  });
-  if (isLeft(eitherUserAuthentication)) {
-    return eitherUserAuthentication.left;
-  }
+  // const cookie = cookies().get("session")?.value || "";
+  // const eitherUserAuthentication = await serverAdapter.userAuthentication({
+  //   cookie,
+  // });
+  // if (isLeft(eitherUserAuthentication)) {
+  //   return eitherUserAuthentication.left;
+  // }
 
   return await requestRouter.handler(request);
   //! Invoke the usecase
@@ -22,9 +21,4 @@ export async function requestHandler(
   //~ check if the user is authorized to make the request
 
   //~ Check if the request is valid in each usecase
-
-  return NextResponse.json(
-    { message: "Hello World", isAuthenticated: true },
-    { status: 200 }
-  );
 }
