a0176c0c69e251cb9115b92f8c12de34b3cb3e0a
Jason Suarez <swarecito@gmail.com>
2024-05-20 01:22:04 +0200

    refactor: Update LandingHeader component with new logo and navigation links

--- src/app/components/LandingHeader.tsx
+++ src/app/components/LandingHeader.tsx
@@ -1,25 +1,30 @@
-import PaperclipIcon from "@/common/icons/PaperClipIcon";
+import NavbarLogo from "@/common/components/NavBarLogo";
+import { PATHS } from "@/common/constants";
 import Link from "next/link";
 
 export default function LandingHeader() {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center justify-between">
-      <Link className="flex items-center" href="#">
-        <PaperclipIcon className="h-6 w-6" />
-        <span className="ml-2 text-lg font-medium">RequestStuff</span>
-      </Link>
+      <div className="flex items-center">
+        <NavbarLogo />
+      </div>
       <nav className="hidden lg:flex gap-6">
-        <Link className="text-sm font-medium hover:underline" href="#">
+        <Link className="text-sm font-medium hover:underline" href="#features">
           Features
         </Link>
         <Link className="text-sm font-medium hover:underline" href="#">
           Pricing
         </Link>
-        <Link className="text-sm font-medium hover:underline" href="#">
+        <Link className="text-sm font-medium hover:underline" href="#contact">
           Contact
         </Link>
       </nav>
-      <Link href={"/dashboard/requests?"}>Get Started</Link>
+      <Link
+        href={PATHS.DASHBOARD()}
+        className="bg-secondary text-white px-4 py-2 rounded-md hover:bg-blue-600"
+      >
+        Get Started
+      </Link>
     </header>
   );
 }
