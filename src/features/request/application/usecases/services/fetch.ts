2984849396219b048bc34f2710d6b6522029cfea
Jason Suarez <swarecito@gmail.com>
2024-05-19 17:02:47 +0200

    refactor: Update fetchServer function to handle server response validation

--- src/features/request/application/usecases/services/fetch.ts
+++ src/features/request/application/usecases/services/fetch.ts
@@ -1,6 +1,11 @@
 import { ServerRequestBodySchema } from "@/app/api/requests/server-request-schema";
 import { ServerEndpointsKeys, getServerEndpoint } from "@/common/constants";
-import { ServerReturnTypes } from "@/features/server/domain/return-types";
+import {
+  ServerReturnTypes,
+  serverReturnTypesSchema,
+} from "@/features/server/domain/return-types";
+import { Failure } from "fp-ddd";
+import { Either, left, right } from "fp-ts/lib/Either";
 
 export function fetchBody(options: ServerRequestBodySchema) {
   return JSON.stringify({
@@ -19,11 +24,26 @@ export async function fetchServer({
   method?: "POST" | "GET" | "PUT" | "DELETE";
   headers?: Headers;
   bodyOptions: ServerRequestBodySchema;
-}): Promise<ServerReturnTypes> {
-  return fetch(getServerEndpoint(path), {
+}): Promise<Either<Failure<string>, ServerReturnTypes>> {
+  const response = await fetch(getServerEndpoint(path), {
     method,
     headers,
     body: fetchBody(bodyOptions),
     //! Create the zod schema for each serverusecase method return type so that we can validate the response instead of just assuming it's correct
-  }).then((response) => response.json() as Promise<ServerReturnTypes>);
+    //! Also create a zod schema for the server request body so that we can validate the request before sending it
+    //? Also type the return type of the fetchServer function to be the return type of the server usecase method
+  });
+  const data = await response.json();
+  console.log({ data });
+  const chekedData = serverReturnTypesSchema.safeParse(data);
+  if (chekedData.success) {
+    return right(chekedData.data);
+  } else {
+    return left(
+      Failure.invalidValue({
+        invalidValue: chekedData.error.errors.join(", "),
+        message: "Invalid response from server",
+      })
+    );
+  }
 }
