1c59cf4649fa7236708366ef39b7f69680bcb276
Jason Suarez <swarecito@gmail.com>
2024-05-19 17:01:25 +0200

    feat: Add server return types and schemas for getUserRequests and registerRequest

--- src/features/server/domain/return-types.ts
+++ src/features/server/domain/return-types.ts
@@ -1,3 +1,26 @@
-import ServerUsecases from "../application/usecases/server-usecases"
+import { privateRequestSchema } from "@/features/request/domain/entities/request-schema";
+import ServerUsecases from "../application/usecases/server-usecases";
+import { z } from "zod";
 
-export type ServerReturnTypes = ReturnType< ServerUsecases["registerRequest"]> | ReturnType< ServerUsecases["getUserRequests"]>  
+// export type ServerReturnTypes =
+//   | ReturnType<ServerUsecases["registerRequest"]>
+//   | ReturnType<ServerUsecases["getUserRequests"]>;
+
+export const baseServerReturnTypeSchema = z.object({
+  error: z.boolean(),
+  message: z.string(),
+});
+
+export const serverGetUserRequestsReturnTypeSchema = z.object({
+  requests: z.array(privateRequestSchema),
+});
+
+export const serverRegisterRequestReturnTypeSchema = baseServerReturnTypeSchema;
+
+export const serverReturnTypesSchema = baseServerReturnTypeSchema.extend({
+  payload: 
+    serverGetUserRequestsReturnTypeSchema,
+ 
+});
+
+export type ServerReturnTypes = z.infer<typeof serverReturnTypesSchema>;
