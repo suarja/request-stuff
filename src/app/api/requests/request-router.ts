3c244b9205454dd105721f891b048d263854ee7a
Jason Suarez <swarecito@gmail.com>
2024-05-19 17:40:30 +0200

    refactor: Update request-router to handle different request targets

--- src/app/api/requests/request-router.ts
+++ src/app/api/requests/request-router.ts
@@ -15,66 +15,73 @@ export default class RequestRouter {
   }
 
   async handler(req: NextRequest): Promise<NextResponse> {
-    const reqJson = await req.json();
-    const body = baseServerRequestBodySchema.safeParse(reqJson);
-    if (body.success) {
-      switch (body.data.target) {
-        case "addPublicRequest":
-          const payload =
-            serverRequestBodySchemaAddPublicRequest.safeParse(reqJson);
-          if (payload.success) {
-            //! Type next response type generic
-            return this._serverUsecases.addPublicRequest({
-              request: payload.data.payload.request,
-            });
-          } else {
+    try {
+      const reqJson = await req.json();
+      const body = baseServerRequestBodySchema.safeParse(reqJson);
+      if (body.success) {
+        switch (body.data.target) {
+          case "addPublicRequest":
+            const payload =
+              serverRequestBodySchemaAddPublicRequest.safeParse(reqJson);
+            if (payload.success) {
+              //! Type next response type generic
+              return this._serverUsecases.addPublicRequest({
+                request: payload.data.payload.request,
+              });
+            } else {
+              return NextResponse.json(
+                { error: "Invalid request body", errorInfo: payload.error },
+                { status: 400 }
+              );
+            }
+          case "getRequestsByUser":
+            const userRequestspayload =
+              serverRequestBodySchemaGetUserRequests.safeParse(reqJson);
+            if (userRequestspayload.success) {
+              return this._serverUsecases.getUserRequests({
+                userId: userRequestspayload.data.payload.userId,
+              });
+            } else {
+              return NextResponse.json(
+                {
+                  error: "Invalid request body",
+                  errorInfo: userRequestspayload.error,
+                },
+                { status: 400 }
+              );
+            }
+
+          case "updatePublicRequest":
             return NextResponse.json(
-              { error: "Invalid request body", errorInfo: payload.error },
+              { message: "update public request" },
+              { status: 200 }
+            );
+          case "updatePublicRequestUploads":
+            return NextResponse.json(
+              { message: "update public request uploads" },
+              { status: 200 }
+            );
+
+          case "deletePublicRequest":
+            return NextResponse.json(
+              { message: "delete public request" },
+              { status: 200 }
+            );
+          default:
+            return NextResponse.json(
+              { error: "Invalid target" },
               { status: 400 }
             );
-          }
-        case "getRequestsByUser":
-          const userRequestspayload =
-            serverRequestBodySchemaGetUserRequests.safeParse(reqJson);
-          if (userRequestspayload.success) {
-            return this._serverUsecases.getUserRequests({
-              userId: userRequestspayload.data.payload.userId,
-            });
-          } else {
-            return NextResponse.json(
-              {
-                error: "Invalid request body",
-                errorInfo: userRequestspayload.error,
-              },
-              { status: 400 }
-            );
-          }
-
-        case "updatePublicRequest":
-          return NextResponse.json(
-            { message: "update public request" },
-            { status: 200 }
-          );
-        case "updatePublicRequestUploads":
-          return NextResponse.json(
-            { message: "update public request uploads" },
-            { status: 200 }
-          );
-
-        case "deletePublicRequest":
-          return NextResponse.json(
-            { message: "delete public request" },
-            { status: 200 }
-          );
-        default:
-          return NextResponse.json(
-            { error: "Invalid target" },
-            { status: 400 }
-          );
+        }
+      } else {
+        return NextResponse.json(
+          { error: "Invalid request body", errorInfo: body.error },
+          { status: 400 }
+        );
       }
-    } else {
+    } catch (error) {
       return NextResponse.json(
-        { error: "Invalid request body", errorInfo: body.error },
+        { error: "Invalid request body", errorInfo: error },
         { status: 400 }
       );
     }
