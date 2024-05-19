be73fb42cc1459c16187ba28c6a89a2a29344a7f
Jason Suarez <swarecito@gmail.com>
2024-05-19 17:55:20 +0200

    refactor: Update middleware to handle API requests and session authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,27 +2,56 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { BASE_URL } from "./common/constants";
 
+function isApiRequest(urlPathname: string) {
+  console.log(urlPathname);
+  return urlPathname.startsWith("/api");
+}
+
 export async function middleware(request: NextRequest, response: NextResponse) {
-  const session = request.cookies.get("session");
+  try {
+    const session = request.cookies.get("session");
+    if (!session) {
+      console.log("No session");
+      // Check if the request is an API request
+      const { pathname } = request.nextUrl;
+      if (isApiRequest(pathname)) {
+        const authHeader = request.headers.get("Authorization");
+        if (authHeader) {
+          const uid = process.env.API_UUID;
+          if (authHeader === `Bearer ${uid}`) {
+            return NextResponse.next();
+          } else {
+            return NextResponse.json(
+              {
+                error: true,
+                message: "Unauthorized",
+              },
+              { status: 401 }
+            );
+          }
+        }
+        return NextResponse.next();
+      }
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-  //Return to /login if don't have a session
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    //Call the authentication endpoint
+    const responseAPI = await fetch(`${BASE_URL}/api/login`, {
+      headers: {
+        Cookie: `session=${session?.value}`,
+      },
+    });
+
+    //Return to /login if token is not authorized
+    if (responseAPI.status !== 200) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json({ error }, { status: 401 });
   }
-
-  //Call the authentication endpoint
-  const responseAPI = await fetch(`${BASE_URL}/api/login`, {
-    headers: {
-      Cookie: `session=${session?.value}`,
-    },
-  });
-
-  //Return to /login if token is not authorized
-  if (responseAPI.status !== 200) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  return NextResponse.next();
 }
 
 //Add your protected routes
@@ -32,5 +61,6 @@ export const config = {
     "/admin/:path*",
     "/dashboard/:path*",
     "/requests/:path*",
+    "/api/requests/:path*", //Add the API routes that you want to protect
   ],
 };
