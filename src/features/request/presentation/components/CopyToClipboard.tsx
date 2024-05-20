6b1e8c7208e7d1d57a3e0b6db9c300ab3cdb66d1
Jason Suarez <swarecito@gmail.com>
2024-05-20 13:28:24 +0200

    refactor: Update CopyToClipboard component with CustomDialog and useGetRandomShortenUrl hook

--- src/features/request/presentation/components/CopyToClipboard.tsx
+++ src/features/request/presentation/components/CopyToClipboard.tsx
@@ -1,5 +1,8 @@
 import { toast } from "sonner";
 import CopyToClipBoardIcon from "../../../../common/icons/CopyToClipboard";
+import CustomDialog from "@/common/components/CustomDialog";
+import { Button } from "@/common/components/ui/button";
+import useGetRandomShortenUrl from "../hooks/useRandomShortenUrl";
 
 export function CopyToClipBoard({
   url,
@@ -8,15 +11,48 @@ export function CopyToClipBoard({
   url: string;
   props?: React.ComponentProps<"button">;
 }) {
+  const {
+    setUrl,
+  } = useGetRandomShortenUrl();
+
   return (
-    <button
-      {...props}
-      onClick={() => {
-        navigator.clipboard.writeText(url);
-        toast.success("Copied to clipboard");
-      }}
-    >
-      <CopyToClipBoardIcon />
-    </button>
+    <>
+      <CustomDialog
+        title="Get URL"
+        icon={<CopyToClipBoardIcon />}
+        buttonClassName="bg-transparent"
+        description="Click the button below to copy the URL to your clipboard."
+      >
+        <div className="flex items-start flex-col gap-4 py-6">
+          <div className="flex items-center">
+            <button
+              {...props}
+              onClick={() => {
+                navigator.clipboard.writeText(url);
+                toast.success("Copied to clipboard");
+              }}
+              className="flex items-center gap-2 text-blue-700 hover:text-blue-400 font-extrabold  rounded-md"
+            >
+              <CopyToClipBoardIcon />
+            </button>
+            <span
+              style={{
+                paddingLeft: "10px",
+                overflowWrap: "break-word",
+                wordWrap: "break-word",
+                wordBreak: "break-word",
+              }}
+            >
+              {url.length > 50 ? `${url.slice(0, 50)}...` : url}
+            </span>
+          </div>
+          <div className="">
+            {/* Short URL  */}
+            <Button onClick={() => setUrl(url)}>Shorten URL</Button>
+            {/* Custom short URL */}
+          </div>
+        </div>
+      </CustomDialog>
+    </>
   );
 }
