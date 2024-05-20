60185de7b660381c361e653765ca2062f8a06d6b
Jason Suarez <swarecito@gmail.com>
2024-05-20 19:10:34 +0200

    refactor: Update RootLayout component to add viewport meta tag for better responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
