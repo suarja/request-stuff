30eee81433b1401a02a5de6ef21f6f7d8372fb91
Jason Suarez <swarecito@gmail.com>
2024-05-20 18:48:35 +0200

    refactor: Update UploadView component to use Input component for file selection

--- src/features/request/presentation/views/UploadView.tsx
+++ src/features/request/presentation/views/UploadView.tsx
@@ -6,6 +6,7 @@ import { PageProps } from "@/app/upload/[slug]/page";
 import CustomDialog from "@/common/components/CustomDialog";
 import SenderMetadataForm from "../components/RequestSenderMetadataForm";
 import NavbarLogo from "@/common/components/NavBarLogo";
+import { Input } from "@/common/components/ui/input";
 
 const UploadView: React.FC<PageProps> = ({ searchParams }) => {
   const [fileSelected, setFileSelected] = useState<File | null>(null);
@@ -25,53 +26,49 @@ const UploadView: React.FC<PageProps> = ({ searchParams }) => {
           <div className="flex justify-between">
             <NavbarLogo />
           </div>
-         
         </div>
       </CardHeader>
       <CardContent className="p-4 space-y-4 w-full">
-          <dl className="pt-4 px-4 w-full grid grid-cols-2 gap-4 text-sm">
-            <dt className="font-semibold">Name</dt>
-            <dd className="">
-              <span className="font-medium">{searchParams?.requestName}</span>
-            </dd>
-            {searchParams?.requestDescription && (
-              <>
-                <dt className="font-semibold">Description</dt>
-                <dd className="w-full">
-                  <span className="font-medium w-full">
-                    {searchParams?.requestDescription}
-                  </span>
-                </dd>
-              </>
-            )}
-            <dt className="font-semibold">Max file size</dt>
-            <dd className="">
-              <span className="font-medium">
-                {searchParams?.maxFileSize} MB
-              </span>
-            </dd>
+        <dl className="pt-4 px-4 w-full grid grid-cols-2 gap-4 text-sm">
+          <dt className="font-semibold">Name</dt>
+          <dd className="">
+            <span className="font-medium">{searchParams?.requestName}</span>
+          </dd>
+          {searchParams?.requestDescription && (
+            <>
+              <dt className="font-semibold">Description</dt>
+              <dd className="w-full">
+                <span className="font-medium w-full">
+                  {searchParams?.requestDescription}
+                </span>
+              </dd>
+            </>
+          )}
+          <dt className="font-semibold">Max file size</dt>
+          <dd className="">
+            <span className="font-medium">{searchParams?.maxFileSize} MB</span>
+          </dd>
 
-            <dt className="font-semibold">Date limit</dt>
-            <dd className="">
-              <span className="font-medium">
-                {new Date(
-                  (searchParams?.dateLimit as string) ?? new Date()
-                ).toDateString()}
-              </span>
-            </dd>
-          </dl>
-        <form className=" gap-4 pt-4 px-4 flex flex-col justify-between">
-            <label className="text-sm" htmlFor="files">
-              Select the files to upload
-            </label>
-            <input
-              onChange={handleFileChange}
-              type="file"
-              id="files"
-              name="files"
-              multiple
-              className="block cursor-pointer w-full text-sm file:border file:border-gray-800 file:px-4 file:py-1 file:rounded-lg file:text-sm file:font-semibold file:bg-primary file:hover:file:bg-blue-600"
-            />
+          <dt className="font-semibold">Date limit</dt>
+          <dd className="">
+            <span className="font-medium">
+              {new Date(
+                (searchParams?.dateLimit as string) ?? new Date()
+              ).toDateString()}
+            </span>
+          </dd>
+        </dl>
+        <form className=" gap-2 pt-4 px-4 flex flex-col justify-between ">
+          <label className="text-sm" htmlFor="files">
+            Select the files to upload
+          </label>
+          <Input
+            onChange={handleFileChange}
+            type="file"
+            id="files"
+            name="files"
+            className="border border-gray-600"
+          />
           <div className="flex justify-center">
             {" "}
             {fileSelected && (
