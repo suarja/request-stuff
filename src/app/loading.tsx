59535fb6c1529967c0f14d29903659f217325153
Jason Suarez <swarecito@gmail.com>
2024-05-20 13:46:55 +0200

    refactor: Update LoadingSkeletonPage component with dark mode support

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,4 @@
 import LoadingSkeletonPage from "@/common/components/LoadingSkeletonPage";
-import { Skeleton } from "@/common/components/ui/skeleton";
 
 export default async function Page() {
   return <LoadingSkeletonPage />;
--- src/common/components/LoadingSkeletonPage.tsx
+++ src/common/components/LoadingSkeletonPage.tsx
@@ -3,48 +3,48 @@ import { Skeleton } from "./ui/skeleton";
 export default function LoadingSkeletonPage() {
   return (
     <div className="flex flex-col ml-8 justify-between mx-auto items-center py-4 h-screen">
-      <Skeleton className="h-10 w-[80%] mt-2 bg-gray-800" />
+      <Skeleton className="h-10 w-[80%] mt-2 dark:bg-gray-800 bg-gray-300 " />
       <div>
-        <Skeleton className="h-[45%] w-[80%] rounded-xl bg-gray-800" />
+        <Skeleton className="h-[45%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
       </div>
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
       <div className="flex  items-center space-x-4 ">
-        <Skeleton className="h-12 w-12 rounded-full bg-gray-800" />
+        <Skeleton className="h-12 w-12 rounded-full dark:bg-gray-800 bg-gray-300 " />
         <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px] bg-gray-800" />
-          <Skeleton className="h-4 w-[200px] bg-gray-800" />
+          <Skeleton className="h-4 w-[250px] dark:bg-gray-800 bg-gray-300 " />
+          <Skeleton className="h-4 w-[200px] dark:bg-gray-800 bg-gray-300 " />
         </div>
       </div>
 
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[10.5%] w-[50%] rounded-full bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[10.5%] w-[50%] rounded-full dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
 
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
       <div className="grid grid-cols-2">
         <div className="flex  items-end justify-center space-x-4 ">
-          <Skeleton className="h-12 w-12 rounded-full bg-gray-800" />
+          <Skeleton className="h-12 w-12 rounded-full dark:bg-gray-800 bg-gray-300 " />
           <div className="space-y-2">
-            <Skeleton className="h-4 w-[250px] bg-gray-800" />
-            <Skeleton className="h-4 w-[200px] bg-gray-800" />
+            <Skeleton className="h-4 w-[250px] dark:bg-gray-800 bg-gray-300 " />
+            <Skeleton className="h-4 w-[200px] dark:bg-gray-800 bg-gray-300 " />
           </div>
         </div>
       </div>
       <div className="flex  items-end justify-center space-x-4 ">
-        <Skeleton className="h-12 w-12 rounded-full bg-gray-800" />
+        <Skeleton className="h-12 w-12 rounded-full dark:bg-gray-800 bg-gray-300 " />
         <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px] bg-gray-800" />
-          <Skeleton className="h-4 w-[200px] bg-gray-800" />
+          <Skeleton className="h-4 w-[250px] dark:bg-gray-800 bg-gray-300 " />
+          <Skeleton className="h-4 w-[200px] dark:bg-gray-800 bg-gray-300 " />
         </div>
       </div>
-      <Skeleton className="h-[1.5%] w-[80%] rounded-xl bg-gray-800" />
+      <Skeleton className="h-[1.5%] w-[80%] rounded-xl dark:bg-gray-800 bg-gray-300 " />
     </div>
   );
 }
