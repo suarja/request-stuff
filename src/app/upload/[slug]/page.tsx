8a9a67488142b787f7f036c54d368aeff692b89a
Jason Suarez <swarecito@gmail.com>
2024-05-28 21:49:59 +0200

    refactor: Add searchParamsHook to UploadView component for better functionality

--- src/app/upload/[slug]/page.tsx
+++ src/app/upload/[slug]/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import UploadView from "@/features/request/presentation/views/UploadView";
-
+import { useSearchParams } from "next/navigation";
 export interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 export default function Page({ searchParams }: PageProps) {
+  const searchParamsHook = useSearchParams();
+  console.log({ searchParams });
+  console.log({ searchParamsHook });
   return <UploadView searchParams={searchParams} />;
 }
